e83af72201a51b9c1e232e659c68c80d8a919205
ducnghu <55783340+ducnghu@users.noreply.github.com>
2020-12-01 16:37:14 +0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,10 @@
 
 const mongoose = require("mongoose")
-
+const dev_db_url = "mongodb+srv://ducnghu:ducnghu18120165@cluster0.3cbcn.mongodb.net/ClothesStore?retryWrites=true&w=majority"
+const mongodb = process.env.MONGODB_URI || dev_db_url;
 async function connect(){
     try {
-        await mongoose.connect("mongodb+srv://ducnghu:ducnghu18120165@cluster0.3cbcn.mongodb.net/ClothesStore?retryWrites=true&w=majority",{
+        await mongoose.connect(mongodb,{
             //   useNewUrlParser: true,
             //   useUnifiedTopology: true,
             //   useCreateIndex: true,
