275c4c50d6115d0b4877b7909cbf33be14ecc389
ducnghu <55783340+ducnghu@users.noreply.github.com>
2020-12-01 18:34:51 +0700

    Update productsController.js

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,7 +2,7 @@ const productsModel = require('../models/productsModel');
 
 exports.index = (req, res, next) => {
     const productlist = productsModel.find({})
-        .then(products => console.log(products))
+        .then(products => res.render("products/products", {products}))
         .catch(next)
 };
 
